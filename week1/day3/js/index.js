be52f2ca72ec03f9937de4c8d89f5d0b2b3126e2
Dire-dragon <1536175744@qq.com>
2021-10-13 09:39:13 +0800

    dom-properties

---
+++ week1/day3/js/index.js
@@ -0,0 +1,6 @@
+const dom = document.querySelector('.filter-item');
+console.log(Array.isArray(dom));
+console.log(dom.innerHTML);
+console.log(dom.textContent);
+console.log(dom);
+console.log(dom.className);
